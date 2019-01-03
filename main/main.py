2c6dd5077a505f16caa48515b0bec438ed6d69b9
MichaelEE0318 <y2010yam@gmail.com>
2019-01-03 14:34:46 +0800

    v1.0

---
+++ main/main.py
@@ -0,0 +1,13 @@
+r=0
+
+for num in range(0,50,1):
+    r+=1
+    if num > 40 :
+        print('Help')
+    else:
+        print('Hello World!')
+    
+    
+
+print(r)
+print(num)
---
+++ test.py
@@ -0,0 +1,14 @@
+print('Hello here!')
+r=0
+
+for num in range(0,50,1):
+    r+=1
+    if num > 40 :
+        print('Help')
+    else:
+        print('Hello World!')
+    
+    
+
+print(r)
+print(num)
